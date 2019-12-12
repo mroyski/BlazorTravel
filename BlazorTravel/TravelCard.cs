a791ee2fbb635ebe6e0065d7c5549fdc235df376
Michael Royski <mroyski@gmail.com>
2019-12-12 15:01:40 -0500

    added travel page and some basic data

---
+++ BlazorTravel/TravelCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTravel
+{
+    public class TravelCard
+    {
+        public int TravelCardId { get; set; }
+        public string Destination { get; set; }
+        public string Description { get; set; }
+        public int NumberOfDays { get; set; }
+        public decimal Price { get; set; }
+    }
+}
